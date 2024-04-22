0bb8b50d11c6f21316850af30f8bc5d5a8e40568
AleGomezRios44 <nicogr0809@gmail.com>
2024-04-22 15:04:37 -0300

    Agrupación de intervalos en teoría anda

--- TP2/ChiCuadrado.cs
+++ TP2/ChiCuadrado.cs
@@ -182,7 +182,41 @@ public class ChiCuadradoTest
             fe[i] = poi * N;
         }
 
+        List<double> feAcum = new List<double>();
+        List<double[,]> intervalosNuevos = new List<double[,]>();
+
+        double feSum = 0;
+        double limiteInferior = 0;
+        double limiteSuperior = 0;
         //ACA DEBE IR EL AGRUPAMIENTO
+        for(int i = 0; i < cantIntervalos; i++)
+        {
+            if (fe[i] < 5)
+            {
+                feSum = feSum + fe[i];
+            }
+            if (limiteInferior == 0)
+            {
+                limiteInferior = intervalos[i, 1];
+            }
+            else
+            {
+                if(limiteInferior < intervalos[i, 0] && feSum >= 5)
+                {
+                    limiteSuperior = intervalos[i, 1];
+                    feAcum.Add(feSum);
+                    intervalosNuevos.Add(new double[,] { { limiteInferior, limiteSuperior } });
+                    feSum = 0;
+                    limiteInferior = 0;
+
+                }
+            }
+            if(i+1 == cantIntervalos && feSum > 0)
+            {
+                feAcum[i - 1] += feSum;
+                intervalosNuevos[i - 1][0, 1] = intervalos[i, 1];
+            }
+        }
 
         List<int> fo = new List<int>();
 
